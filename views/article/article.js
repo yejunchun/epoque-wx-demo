4fce404670fcbef0e67381a845839f2d92c9656c
叶俊宏 <yejunchun@yjh.local>
2018-05-18 06:57:30 +0800

    .

--- views/article/article.js
+++ views/article/article.js
@@ -1,13 +1,17 @@
-// pages/login.js
+// pages/login/login.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    logoLoginSrc: '../../resources/logo-login.png'
   },
-
+  login: function(){
+    wx.redirectTo({
+      url: '../index/index',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
