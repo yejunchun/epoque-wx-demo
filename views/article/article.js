8dfd6e200bfe164920c4401359b5a29d67119ee4
叶俊宏 <yejunchun@yjh.local>
2018-05-20 21:35:46 +0800

    新增设置，知识库

--- views/article/article.js
+++ views/article/article.js
@@ -1,70 +1,60 @@
 // pages/login/login.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    logoLoginSrc: '../../resources/logo-login.png'
+    nav: false,
+    animationData: {},
+    animationOpacity: {},
+    animation: {
+      nav: {},
+      bak: {}
+    }
   },
-  login: function(){
-    wx.redirectTo({
-      url: '../index/index',
+  clickNav: function() {
+    let nav = !this.data.nav?true:false
+
+    let animationHeight = this.data.nav == true?-200:0;
+    let opacity = this.data.nav == false ? 1 : 0;
+
+    setTimeout(function () {
+      this.data.animation.nav.translateY(animationHeight).step()
+      this.data.animation.bak.opacity(opacity).step()
+      this.setData({
+        animationData: this.data.animation.nav.export(),
+        animationOpacity: this.data.animation.bak.export(),
+      })
+    }.bind(this), 0)  
+
+    if (this.data.nav == false){
+      this.setData({ nav: true })
+    }else{
+      setTimeout(function () {
+        this.setData({
+          nav: false
+        })
+      }.bind(this), 200)
+    }
+  },
+  createAnimation(){
+    let animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "ease-out",
+      delay: 0
     })
+    return animation
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+  onLoad(){
+    this.data.animation.nav = this.createAnimation()
+    this.data.animation.bak = this.createAnimation()
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    this.data.animation.nav.translateY(-300).step()
+    this.data.animation.bak.opacity(0).step()
+    this.setData({
+      animationData: this.data.animation.nav.export(),
+      animationOpacity: this.data.animation.bak.export(),
+    })
+    
   }
 })
--- views/index/index.js
+++ views/index/index.js
@@ -1,6 +1,5 @@
 //index.js
 //获取应用实例
-
 Page({
   data: {
     name: '店员名称',
@@ -9,6 +8,8 @@ Page({
     shopAddress: '广东深圳市罗湖区田心村'
   },
   onLoad: function () {
-   
+    wx.setNavigationBarTitle({
+      title: '当前页面'
+    })
   }
 })
